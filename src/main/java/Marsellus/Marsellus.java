04f2433476a45801add0a2fd0a26da0e403cfca2
paxon <alexander.simakin@gmail.com>
2016-09-04 23:39:27 +0300

    Naan

--- src/main/java/Marsellus/Marsellus.java
+++ src/main/java/Marsellus/Marsellus.java
@@ -5,6 +5,16 @@ import java.util.*;
 /**
  * Created by HP on 28.08.2016.
  */
+
+/**
+ * This is a simple plan for computer related stuff wholesaler. It transforms consumption requests from customers of
+ * different levels to purchase plan. Some basic production, logistic and financial conditions are considered at the
+ * beginning of this small tutorial project. An attempt to consider end-user behaviour is also made.
+ *
+ * We are a wholesaler who tries to run an eye over our plan of importing electronic goods. We will try to account
+ * profits and expenses.
+ */
+
 public class Marsellus {
 
     public static void main(String[] args)
